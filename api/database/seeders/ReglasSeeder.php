40a32ff57ede3f218e3bd49509ffd544592cdf7c
JOlave <jaime.olave@usm.cl>
2024-06-04 16:49:51 -0400

    Controllers

---
+++ api/database/seeders/ReglasSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReglasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
