5c200bf55725f21e7c686bdde12573e816982a2d
JOlave <jaime.olave@usm.cl>
2024-06-05 17:22:43 -0400

    homepage

---
+++ appcer2/lib/pages/tabs/campeonatos.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class  CampeonatosTab extends StatelessWidget {
+  const CampeonatosTab ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
