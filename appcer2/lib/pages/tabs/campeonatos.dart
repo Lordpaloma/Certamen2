7fda0f03f636286a1a8a539398b5a3ef343d8451
JOlave <jaime.olave@usm.cl>
2024-06-05 21:53:45 -0400

    listview y tiles a 70%?

--- appcer2/lib/pages/tabs/campeonatos.dart
+++ appcer2/lib/pages/tabs/campeonatos.dart
@@ -1,18 +1,23 @@
+import 'package:appcer2/services/http_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class  CampeonatosTab extends StatelessWidget {
+class CampeonatosTab extends StatefulWidget {
+  @override
+  State<CampeonatosTab> createState() => _CampeonatosTabState();
+}
+
+class _CampeonatosTabState extends State<CampeonatosTab> {
   final AssetImage fondo = AssetImage('assets/images/campeonatos.jpg');
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
+      body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(image: fondo, fit: BoxFit.cover)
+          image: DecorationImage(image: fondo, fit: BoxFit.cover),
         ),
-      )
+      ),
     );
   }
 }
--- appcer2/lib/services/http_service.dart
+++ appcer2/lib/services/http_service.dart
@@ -5,9 +5,22 @@ import 'package:http/http.dart' as http;
 class HttpService {
   final String apiUrl = 'http://10.0.2.2:8000/api';
 
-  Future<List<dynamic>> equipos() async {
+  Future<List<dynamic>> campeonatos() async {
+    return listarDatos('campeonatos');
+  }
+
+    Future<List<dynamic>> equipos() async {
     return listarDatos('equipos');
   }
+
+      Future<List<dynamic>> jugadores() async {
+    return listarDatos('jugadores');
+  }
+
+      Future<List<dynamic>> partidos() async {
+    return listarDatos('partidos');
+  }
+
 
     Future<List<dynamic>> listarDatos(String coleccion) async {
     var respuesta = await http.get(Uri.parse(apiUrl + '/' + coleccion));
