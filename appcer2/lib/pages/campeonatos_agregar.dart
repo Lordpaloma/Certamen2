f901fd78873514c132c0825d6effbdcdcfc3eecc
JOlave <jaime.olave@usm.cl>
2024-06-09 16:26:57 -0400

    Update campeonatos_agregar.dart

--- appcer2/lib/pages/campeonatos_agregar.dart
+++ appcer2/lib/pages/campeonatos_agregar.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CampeonatosAgregar extends StatelessWidget {
+class CampeonatosAgregar extends StatefulWidget {
   const CampeonatosAgregar({super.key});
 
+  @override
+  State<CampeonatosAgregar> createState() => _CampeonatosAgregarState();
+}
+
+class _CampeonatosAgregarState extends State<CampeonatosAgregar> {
   @override
   Widget build(BuildContext context) {
     return Container();
