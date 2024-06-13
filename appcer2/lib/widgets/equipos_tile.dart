3e9675a6aeef6cea8c6e63d7016ff6439df3fd51
PaulinaCandia <paulina.candia@sansano.usm.cl>
2024-06-12 23:57:38 -0400

    tap tap tap

--- appcer2/lib/widgets/equipos_tile.dart
+++ appcer2/lib/widgets/equipos_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:appcer2/services/http_service.dart';
 
+
 class EquiposTile extends StatefulWidget {
   final String nombre;
   final String acronimo;
@@ -9,7 +10,8 @@ class EquiposTile extends StatefulWidget {
   EquiposTile(
       {this.nombre = '-',
       this.acronimo = 'xx',
-      this.entrenador ='-'});
+      this.entrenador ='-'}
+      );
 
   @override
   State<EquiposTile> createState() => _EquiposTileState();
@@ -18,35 +20,43 @@ class EquiposTile extends StatefulWidget {
 class _EquiposTileState extends State<EquiposTile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.fromLTRB(5, 5, 5, 2),
-      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.white60),
-        color: Color.fromARGB(173, 255, 255, 255),
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(Icons.gamepad_sharp),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text('${this.widget.nombre} | ',
-                  style: TextStyle(fontSize:16),),
-                  Text(
-                    '${this.widget.acronimo}',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                  
-                ],
-              )
-            ],
-          )
-        ],
+    
+    return InkWell(
+      onTap: () => print("Container pressed"), // Handle your onTap here.
+      child: Container(
+        margin: EdgeInsets.fromLTRB(5, 5, 5, 2),
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+        
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white60),
+          color: Color.fromARGB(173, 255, 255, 255),
+        ),
+        
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          
+          children: [
+            Icon(Icons.gamepad_outlined),
+          
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text('${this.widget.nombre} | ',
+                    style: TextStyle(fontSize:16),),
+                    Text(
+                      '${this.widget.acronimo}',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
+    
   }
 }
