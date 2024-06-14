19def529fd6a842a383e898cf2e7fdbbc0c16379
JOlave <jaime.olave@usm.cl>
2024-06-14 19:37:59 -0400

    .

--- appcer2/lib/pages/tabs/calendario.dart
+++ appcer2/lib/pages/tabs/calendario.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-class CalendarioTab extends StatelessWidget {
+class CalendarioTab extends StatefulWidget {
   const CalendarioTab({super.key});
 
   @override
+  State<CalendarioTab> createState() => _CalendarioTabState();
+}
+
+class _CalendarioTabState extends State<CalendarioTab> {
+  final AssetImage fondo = AssetImage('assets/images/campeonatos.jpg');
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: fondo, fit: BoxFit.cover),
+        ),
+      ),
+    );
   }
 }
